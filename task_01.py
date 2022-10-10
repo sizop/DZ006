efce542249d254e1fe02e3a0a7fcb180fa2b3c97
Dmirty <9048881951@mail.ru>
2022-10-10 16:33:55 +0500

    01

--- task_01.py
+++ task_01.py
@@ -13,3 +13,20 @@
 # out
 # [28, 20, 10, 5, 1, 24, 7, 15, 23, 25]
 # [24, 15, 23, 25]
+
+from random import randint
+
+
+def new_list(num=20):
+    new_list = [randint(2, 100) for i in range(num)]
+    print(*f'Входящий список:\n{new_list}',sep='')
+    return new_list
+
+
+def out_list(in_list=new_list()):
+    out_list = list(map(lambda x: x // 1, in_list))
+    out_list = [in_list[i] for i in range(1, len(in_list)) if int(in_list[i]-1) < int(in_list[i])]
+    return out_list
+
+
+print(*f'Исходящий список:\n{out_list()}',sep='')
