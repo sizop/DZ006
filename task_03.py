12d4c61d3a3d53d0b0d74a120d8b727fbe7e9576
Dmirty <9048881951@mail.ru>
2022-10-11 16:24:56 +0500

    01

--- task_03.py
+++ task_03.py
@@ -8,16 +8,14 @@
 # {'А': ['Алина'], 'Б': ['Бибочка'], 'И': ['Иван', 'Илья'], 'М': ['Марина', 'Мария'], 'П': ['Петр', 'Петр']}
 
 def list_staff(staff=["Иван", "Мария", "Петр", "Илья", "Марина", "Петр", "Алина", "Бибочка"], list_keys=[]):
-    for i in range(len(staff)):
-        s = str(staff[i])[0]
-        list_keys.append(s)
-    d_staff = dict(zip(list_keys, staff))
+    d_staff = {}
+    for str in staff:
+        for word in str.split():
+            if d_staff.get(word[0]):
+                d_staff[word[0]] = d_staff[word[0]] + (', ') + (word)
+            d_staff[word[0]] = d_staff.setdefault(word[0], word)
+
     return d_staff
-    # d_staff = {}
-    # for str in staff:
-    #     for word in str.split():
-    #         d_staff[word[0]] = d_staff.setdefault(word[0],word)
-    #
-    # return d_staff
+
 
 print(list_staff())
